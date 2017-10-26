eaed3b7c11d6b2c5b738d5b1404315a6082a0dc7
Buck <bucko.perley@gmail.com>
2017-10-25 20:40:53 -0700

    added router for calls to the bcoin node based on config file

---
+++ server/bcoinRouter.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const path = require('path');
+
+const express = require('express');
+const request = require('request');
+// const auth = require('basic-auth');
+const Client = require('bcoin').http.Client;
+
+const config = require(path.resolve(__dirname, '../bcoin.config.json'));
+
+const bcoinClient = new Client(config);
+const bcoinRouter = express.Router({ mergeParams: true });
+
+// Primary router for preparing the requests to send to bcoin node
+bcoinRouter.use(async (req, res) => {
+  // let authorization;
+
+  // if (auth(req)) {
+  //   authorization = {
+  //     user: auth(req).name,
+  //     pass: auth(req).pass,
+  //     sendImmediately: false,
+  //   };
+  // }
+
+  const options = {
+    method: req.method,
+    uri: req.path,
+    body: req.body,
+    json: true,
+    qs: req.query
+    // auth: authorization,
+  };
+
+  const { method, path, body } = req;
+
+  try {
+    const bcoinResponse = await bcoinClient._request(method, path, body);
+    return res.status(200).json(bcoinResponse);
+  } catch (error) {
+    console.log('got an error querying bcoin node!');
+    return res.status(400).send({ error });
+  }
+});
+
+module.exports = bcoinRouter;
--- server/server.js
+++ server/server.js
@@ -3,7 +3,9 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.set('port', (process.env.PORT || 5000));
+const bcoinRouter = require('./bcoinRouter');
+
+app.set('port', process.env.PORT || 5000);
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../dist')));
@@ -11,6 +13,8 @@ app.use(express.static(path.join(__dirname, '../dist')));
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../webapp/index.html'));
 });
+
+app.use('/node', bcoinRouter);
 
 app.listen(app.get('port'), () => {
   console.log('Node app is running on port', app.get('port')); // eslint-disable-line no-console
