e5c3a28138dd194e002e1a60db27e015ff5238cc
Buck <bucko.perley@gmail.com>
2018-01-20 18:43:55 -0800

    cleanup of server files

--- server/bcoinRouter.js
+++ server/bcoinRouter.js
@@ -1,34 +1,22 @@
 'use strict';
 
-const path = require('path');
 const express = require('express');
-
-const { NodeClient } = require('bclient');
 const logger = require('./logger');
-
-const config = require(path.resolve(__dirname, '../configs/bcoin.config.json'));
-const { network, port, apiKey, host } = config;
-
-const nodeClient = new NodeClient({
-  apiKey,
-  network,
-  host,
-  port: typeof port === 'number' ? port : parseInt(port)
-});
 const bcoinRouter = express.Router({ mergeParams: true });
 
-// Primary router for preparing the requests to send to bcoin node
-bcoinRouter.use(async (req, res, next) => {
-  const { method, path, body } = req;
-  try {
-    const bcoinResponse = await nodeClient.request(method, path, body);
-    return res.status(200).json(bcoinResponse);
-  } catch (error) {
-    logger.error('Error querying bcoin node:', error);
-    const err = new Error('There was a problem querying the bcoin node');
-    err.status = 400;
-    return next(err);
-  }
-});
+const routerWithClient = nodeClient =>
+  // Primary router for preparing the requests to send to bcoin node
+  bcoinRouter.use(async (req, res, next) => {
+    const { method, path, body } = req;
+    try {
+      const bcoinResponse = await nodeClient.request(method, path, body);
+      return res.status(200).json(bcoinResponse);
+    } catch (error) {
+      logger.error('Error querying bcoin node:', error);
+      const err = new Error('There was a problem querying the bcoin node');
+      err.status = 400;
+      return next(err);
+    }
+  });
 
-module.exports = bcoinRouter;
+module.exports = routerWithClient;
--- server/bcoinSocket.js
+++ server/bcoinSocket.js
@@ -1,66 +1,45 @@
-const http = require('http');
-const path = require('path');
-const bsock = require('bsock');
-const io = bsock.createServer();
-const socketServer = http.createServer();
-const { NodeClient } = require('bclient');
-
 const logger = require('./logger');
 
-const config = require(path.resolve(__dirname, '../configs/bcoin.config.json'));
-const { network, port, apiKey, host } = config;
-
-const nodeClient = new NodeClient({
-  apiKey,
-  network,
-  host,
-  port: typeof port === 'number' ? port : parseInt(port)
-});
-
-io.attach(socketServer);
-const subscriptions = {}; // cache to manage subscriptions made by clients
-
-const socketHandler = async socket => {
-  // requests from client for messages to be broadcast to node
-  socket.bind('broadcast', (event, ...args) => {
-    /**
-      // Example broadcast from client:
-      socket.fire('broadcast', 'set filter', '00000000000000000000');
-      // which will result in the following fire to bcoin server
-      nodeClient.socket.fire('set filter', '00000000000000000000');
-    **/
-    logger.info(`Firing "${event}" to bcoin node`);
-    nodeClient.fire(event, ...args);
-  });
-
-  // requests from client to subscribe to events from node
-  socket.bind('subscribe', (event, responseEvent) => {
-    // doing some caching of listeners
-    if (!subscriptions[event]) {
-      subscriptions[event] = [responseEvent]; // cache listener
-    } else if (subscriptions[event].indexOf(responseEvent) === -1) {
-      subscriptions[event].push(responseEvent);
-    }
-
-    logger.debug(`Subscribing to "${event}" event on bcoin node`);
-    nodeClient.bind(event, (...data) => {
-      logger.debug(
-        `Event "${event}" received from node.`,
-        `Firing "${responseEvent}" event`
-      );
-      socket.fire(responseEvent, ...data);
+const socketHandler = nodeClient => {
+  const subscriptions = {}; // cache to manage subscriptions made by clients
+  return async socket => {
+    // requests from client for messages to be broadcast to node
+    socket.bind('broadcast', (event, ...args) => {
+      /**
+        // Example broadcast from client:
+        socket.fire('broadcast', 'set filter', '00000000000000000000');
+        // which will result in the following fire to bcoin server
+        nodeClient.socket.fire('set filter', '00000000000000000000');
+      **/
+      logger.info(`Firing "${event}" to bcoin node`);
+      nodeClient.fire(event, ...args);
     });
-  });
 
-  nodeClient.socket.on('error', err => {
-    logger.error('Socket error: ', err);
-  });
+    // requests from client to subscribe to events from node
+    // client should indicate the event to listen for
+    // and the `responseEvent` to fire when the event is heard
+    socket.bind('subscribe', (event, responseEvent) => {
+      // doing some caching of listeners
+      if (!subscriptions[event]) {
+        subscriptions[event] = [responseEvent]; // cache listener
+      } else if (subscriptions[event].indexOf(responseEvent) === -1) {
+        subscriptions[event].push(responseEvent);
+      }
+
+      logger.debug(`Subscribing to "${event}" event on bcoin node`);
+      nodeClient.bind(event, (...data) => {
+        logger.debug(
+          `Event "${event}" received from node.`,
+          `Firing "${responseEvent}" event`
+        );
+        socket.fire(responseEvent, ...data);
+      });
+    });
+
+    nodeClient.socket.on('error', err => {
+      logger.error('Socket error: ', err);
+    });
+  };
 };
 
-(async function() {
-  await nodeClient.open();
-  nodeClient.fire('auth');
-  io.on('socket', socketHandler);
-})();
-
-module.exports = { socketServer, socketHandler };
+module.exports = socketHandler;
