7fc3ddc385363dcf46f8fe6d5b9eead184d52d19
Buck <bucko.perley@gmail.com>
2018-01-23 13:56:41 -0800

    performance improvement to chain state update

--- webapp/localPlugins/chainSockets/actions.js
+++ webapp/localPlugins/chainSockets/actions.js
@@ -29,12 +29,17 @@ export function setChainTip(entry) {
     let blockMeta = ChainEntry.fromRaw(entry);
     const { time, hash, height } = blockMeta;
     const genesis = getState().chain.genesis.time;
-    let progress = calcProgress(genesis, time);
+    const prevProgress = getState().chain.progress;
+    const progress = calcProgress(genesis, time);
     const chain = { tip: hash, progress, height };
-
-    return dispatch({
-      type: 'SET_CHAIN_INFO',
-      payload: chain
-    });
+    if (progress > 0.9 || progress - prevProgress > 0.0001) {
+      // only update the chain tip if
+      // progress is noticeably different
+      // should resolve some frontend performance issues
+      return dispatch({
+        type: 'SET_CHAIN_INFO',
+        payload: chain
+      });
+    }
   };
 }
