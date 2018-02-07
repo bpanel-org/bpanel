07190ca5696a21fb6a1010af74db9493f387af96
Jonathan Knapp <jonathan@purse.io>
2018-02-06 18:11:53 -0800

    Init secrets.env after install

---
+++ scripts/postinstall.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const crypto = require('crypto');
+const fname = 'secrets.env';
+
+// Init secrets.env
+if (!fs.existsSync(fname)) {
+    fs.writeFileSync(fname,
+        'BCOIN_API_KEY=' + crypto.randomBytes(40).toString('hex') + '\n',
+        {mode: 0o600}
+    );
+}
