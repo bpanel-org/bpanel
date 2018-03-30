7c09caf6326bd5eac63c47298f586dbd5a90a29c
bucko13 <bucko.perley@gmail.com>
2018-03-29 19:16:49 -0700

    remove all local plugins except default

--- webapp/config/pluginsConfig.js
+++ webapp/config/pluginsConfig.js
@@ -1,18 +1,16 @@
-export const localPlugins = [
-  'bpanel-theme',
-  'bui',
-  'bmoon-theme',
-  'bdark-theme'
-];
+export const localPlugins = ['bpanel-theme'];
 
 export const plugins = [
+  '@bpanel/bdark-theme',
+  '@bpanel/bmoon-theme',
   '@bpanel/chain-sockets',
   '@bpanel/dashboard',
   '@bpanel/recent-blocks',
   '@bpanel/peers-widget',
   '@bpanel/mempool-widget',
   '@bpanel/bpanel-footer',
-  '@bpanel/bpanel-header'
+  '@bpanel/bpanel-header',
+  '@bpanel/bui'
 ];
 
 export default { localPlugins, plugins };
