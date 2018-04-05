495bf70b21197e296985d6a20dea52febb602a5c
bucko13 <bucko.perley@gmail.com>
2018-04-05 11:12:41 -0700

    add simple pluginsReducer interface

---
+++ webapp/store/reducers/plugins.js
@@ -0,0 +1,35 @@
+/**
+This is a bare-bones reducer that should be a catch-all
+for custom reducers/state that plugins would like to
+interact with.
+
+When adding, payload should have a unique id for the
+plugin's reducer you want add. If ID already exists,
+it will be skipped.
+**/
+
+import Immutable from 'seamless-immutable';
+
+import { decorateReducer } from '../../plugins/plugins';
+import { ADD_PLUGIN } from '../constants/plugins';
+
+const initialState = Immutable({});
+
+const plugins = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_PLUGIN: {
+      const { id, ...rest } = action.payload;
+      if (state[id])
+        // reducer id already exists
+        // return state without adding reducer
+        // TODO: better reporting of duplicate reducer
+        return state;
+      return state.set(id, { ...rest });
+    }
+
+    default:
+      return state;
+  }
+};
+
+export default decorateReducer(plugins, 'pluginsReducer');
