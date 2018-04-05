572550bcb00dbadf04b4faeec51beb8f750d4a3e
bucko13 <bucko.perley@gmail.com>
2018-04-05 00:26:24 -0700

    remove commented out plugin names

--- webapp/config/pluginsConfig.js
+++ webapp/config/pluginsConfig.js
@@ -1,10 +1,5 @@
 export const localPlugins = ['footer-address', 'wallets'];
 
-export const plugins = [
-  // '@bpanel/bdark-theme',
-  // '@bpanel/bmoon-theme',
-  '@bpanel/simple-mining',
-  '@bpanel/genesis-theme'
-];
+export const plugins = ['@bpanel/genesis-theme'];
 
 export default { localPlugins, plugins };
