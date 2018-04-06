db7c11ec7a34247fbfa2f3c8eb484c10318c1116
mark tyneway <mark@purse.io>
2018-04-06 12:27:35 -0700

    Clean up as to not make linter angry

--- server/bcoinRouter.js
+++ server/bcoinRouter.js
@@ -6,19 +6,17 @@ const logger = require('./logger');
 const routerWithClient = client => {
   const bcoinRouter = express.Router({ mergeParams: true });
 
-  bcoinRouter.all('*', async (req, res, next) => {
+  bcoinRouter.all('*', async (req, res) => {
     const { method, path, body, query } = req;
     // use query params for GET request, otherwise use body
     const payload = method === 'GET' ? query : body;
     try {
       logger.debug(
         `client: ${client.constructor.name}, method: ${method},`,
-        // eslint-disable-next-line
-        `path: ${path}, query:`,
-        query,
-        ', body:',
-        body
+        `path: ${path}`
       );
+      logger.debug('query:', query);
+      logger.debug('body:', body);
       const response = await client.request(method, path, payload);
       logger.debug('server response:', response ? response : 'null');
       if (response) return res.status(200).json(response);
