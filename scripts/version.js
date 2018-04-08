f215f025420a184ee34dfdf588b395f4b776ea58
bucko13 <bucko.perley@gmail.com>
2018-04-08 11:39:12 -0700

    add git fix for version

--- scripts/version.js
+++ scripts/version.js
@@ -1,12 +1,16 @@
 // Looks at the git tags and sha to output the version.
 
+let commit;
 const fs = require('fs');
 const { execSync } = require('child_process');
+try {
+  commit = execSync('git rev-parse HEAD').toString();
+} catch (e) {}
 
 fs.writeFileSync(
   'webapp/version.json',
   JSON.stringify({
-    commit: execSync('git rev-parse HEAD').toString(),
+    commit,
     version: require('../package.json').version
   })
 );
