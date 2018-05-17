d543bdeca57be6ffefa4eb20529321297c17bd55
mark tyneway <mark@purse.io>
2018-05-17 15:10:35 -0700

    Using single async function

--- scripts/docker-bcoin-init.js
+++ scripts/docker-bcoin-init.js
@@ -4,54 +4,53 @@ const bcoin = require('bcoin');
 const fs = require('fs');
 const path = require('path');
 const os = require('os');
+const blgr = require('blgr');
 
-let prefix = process.env.prefix
-  ? process.env.prefix
-  : `${os.homedir()}/.bcoin/`;
-const initScript = process.env.BCOIN_INIT_SCRIPT;
-
-prefix = prefix.replace('~', os.homedir());
-// create the node with our custom configs
-const node = new bcoin.FullNode({ env: true });
-
-// checking if walletdb dir already exists (i.e. was mounted as volume)
-// need to do before using plugin because that will create the dir
-// if not already there
-const fileList = fs.existsSync(prefix) ? fs.readdirSync(prefix) : [];
-const hadWalletDB = fileList.indexOf('wallet') > -1;
-
-if (!node.config.bool('no-wallet') && !node.has('walletdb')) {
-  const walletPlugin = bcoin.wallet.plugin;
-  node.use(walletPlugin);
-}
-
-node.on('error', e => console.error('There was an error: ', e));
+// global variables
+let logger;
+let node;
 
 (async () => {
-  await node.ensure();
-  await node.open();
-  await node.connect();
-  node.startSync();
-})()
-  .then(async () => {
-    console.log('bcoin node is started');
-    // want to set a coinbase address in case of mining
-    const wdb = node.require('walletdb').wdb;
-    const primary = wdb.primary;
-    const coinbase1 = await primary.receiveAddress();
-    const coinbase2 = await primary.receiveAddress();
-    await node.miner.addAddress(coinbase1);
-    await node.miner.addAddress(coinbase2);
+  try {
+    logger = new blgr({
+      level: 'debug'
+    });
+    await logger.open();
+    logger.info('LOGGER OPEN');
+
+    let prefix = process.env.prefix
+      ? process.env.prefix
+      : `${os.homedir()}/.bcoin/`;
+    const initScript = process.env.BCOIN_INIT_SCRIPT;
+
+    prefix = prefix.replace('~', os.homedir());
+    // create the node with our custom configs
+    node = new bcoin.FullNode({ env: true });
+    logger.info('Starting bcoin.FullNode({ env: true })');
+
+    // checking if walletdb dir already exists (i.e. was mounted as volume)
+    // need to do before using plugin because that will create the dir
+    // if not already there
+    const fileList = fs.existsSync(prefix) ? fs.readdirSync(prefix) : [];
+    const hadWalletDB = fileList.indexOf('wallet') > -1;
+
+    node.on('error', e => logger.error('There was an error: ', e));
+
+    await node.ensure();
+    await node.open();
+    await node.connect();
+    node.startSync();
+    logger.info('Starting node sync');
 
     // check if the walletdb exists before running script
-    if (!hadWalletDB && !!initScript) {
-      console.log('No walletdb detected.');
-      console.log(`Running init script ${initScript} now to setup environment`);
+    if (!!initScript) {
+      logger.info('No walletdb detected.');
+      logger.info(`Running init script ${initScript} now to setup environment`);
       await require(path.resolve(__dirname, initScript))(node);
     }
-  })
-  .catch(err => {
-    console.error(err.stack);
+  } catch (e) {
+    logger.error(e.stack);
     node.close();
     process.exit(1);
-  });
+  }
+})();
