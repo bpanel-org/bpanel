e9e25d07797c78c61784a31deac4cea152d228b2
mark tyneway <mark@purse.io>
2018-05-21 13:31:26 -0700

    Start bcoin and bmultisig in different processes
    Need to be sure to pass the right env
    variables in because a client wallet node
    (started by bmultisig) needs specific env
    variables - see:
    https://github.com/bcoin-org/bcoin/blob/c7d844ea3785c939cd546ce2fe7ff0e03f27197a/lib/wallet/node.js
    this.client = new Client({...})

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo "starting init.js and bmultisig"
 # start bcoin and start multisig server
-node /code/scripts/docker-bcoin-init.js & /code/node_modules/bmultisig/bin/bmultisig fg
-
+echo "starting init.js and bmultisig"
+node /code/scripts/docker-bcoin-init.js & /code/node_modules/bmultisig/bin/bmultisig && fg
