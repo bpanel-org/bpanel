d98692442b5e345bedcdc2fbb92098489048063e
mark tyneway <mark@purse.io>
2018-05-21 13:40:06 -0700

    Remove dead code

--- scripts/docker-bcoin-init.js
+++ scripts/docker-bcoin-init.js
@@ -28,12 +28,6 @@ let node;
     node = new bcoin.FullNode({ env: true });
     logger.info('Starting bcoin.FullNode({ env: true })');
 
-    // checking if walletdb dir already exists (i.e. was mounted as volume)
-    // need to do before using plugin because that will create the dir
-    // if not already there
-    const fileList = fs.existsSync(prefix) ? fs.readdirSync(prefix) : [];
-    const hadWalletDB = fileList.indexOf('wallet') > -1;
-
     node.on('error', e => logger.error('There was an error: ', e));
 
     await node.ensure();
@@ -42,9 +36,7 @@ let node;
     node.startSync();
     logger.info('Starting node sync');
 
-    // check if the walletdb exists before running script
     if (!!initScript) {
-      logger.info('No walletdb detected.');
       logger.info(`Running init script ${initScript} now to setup environment`);
       await require(path.resolve(__dirname, initScript))(node);
     }
