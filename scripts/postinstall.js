36e3719333e517c0376f9c2058e409c36c1c0b69
mark tyneway <mark@purse.io>
2018-05-21 14:49:08 -0700

    Add bmultisig env variable

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -2,13 +2,20 @@ const fs = require('fs');
 const crypto = require('crypto');
 const fname = 'secrets.env';
 
+// need to provide the api key
+// to both the bpanel server
+// and the wallet server
+const randomValue = crypto.randomBytes(40).toString('hex');
+
 // Init secrets.env
 if (!fs.existsSync(fname)) {
   fs.writeFileSync(
     fname,
-    'BCOIN_API_KEY=' + crypto.randomBytes(40).toString('hex') + '\n',
+    `BCOIN_API_KEY=${randomValue}
+    BCOIN_NODE_API_KEY=${randomValue}`,
     { mode: 0o600 }
   );
 }
 
+// create webapp/version.json file
 require('./version.js');
