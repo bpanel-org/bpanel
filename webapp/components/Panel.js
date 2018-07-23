98145a6607a0aa746c38e83bdec73ea56d0d3e98
bucko13 <bucko.perley@gmail.com>
2018-07-23 16:15:26 -0700

    remove unsafe

--- webapp/components/Panel.js
+++ webapp/components/Panel.js
@@ -7,7 +7,30 @@ import { getRouteProps } from '../plugins/plugins';
 export default class extends PureComponent {
   constructor(props) {
     super(props);
-    this.routes = [];
+    const { customChildren = [], paths } = props;
+    this.routes = customChildren.map(({ Component, metadata }) => {
+      const { name } = metadata;
+
+      const pathName = paths[name]; // will be a unique path from state
+      let path;
+
+      try {
+        if (!name) throw 'Must pass a name for custom Panels';
+        if (!pathName) {
+          path = encodeURI(name);
+        } else {
+          path = encodeURI(pathName);
+        }
+        return (
+          <Route
+            exact
+            path={`/${path}`}
+            key={`nav-${name}`}
+            render={() => this.childRoute(Component, name)} // using render so we can pass props
+          />
+        );
+      } catch (e) {}
+    });
   }
 
   static get displayName() {
@@ -32,33 +55,6 @@ export default class extends PureComponent {
     const routeProps = getRouteProps(name, this.props);
 
     return <Component {...routeProps} />;
-  }
-
-  UNSAFE_componentWillMount() {
-    const { customChildren = [], paths } = this.props;
-    this.routes = customChildren.map(({ Component, metadata }) => {
-      const { name } = metadata;
-
-      const pathName = paths[name]; // will be a unique from state
-      let path;
-
-      try {
-        if (!name) throw 'Must pass a name for custom Panels';
-        if (!pathName) {
-          path = encodeURI(name);
-        } else {
-          path = encodeURI(pathName);
-        }
-        return (
-          <Route
-            exact
-            path={`/${path}`}
-            key={`nav-${name}`}
-            render={() => this.childRoute(Component, name)} // using render so we can pass props
-          />
-        );
-      } catch (e) {}
-    });
   }
 
   render() {
