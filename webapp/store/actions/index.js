47c3a847d3148f6ac55a0192dfac7ffe0a52a617
bucko13 <bucko.perley@gmail.com>
2018-07-26 11:50:04 -0700

    add new action

--- webapp/store/actions/index.js
+++ webapp/store/actions/index.js
@@ -2,3 +2,4 @@ export { default as nodeActions } from './nodeActions';
 export { default as socketActions } from './socketActions';
 export { default as chainActions } from './chainActions';
 export { default as themeActions } from './themeActions';
+export { default as navActions } from './navActions';
