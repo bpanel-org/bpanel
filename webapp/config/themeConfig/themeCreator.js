2ab2192afa21feee07f078852e99195fd36c6098
mark tyneway <mark@purse.io>
2018-08-20 13:48:37 -0700

    styles: expandedRow updates

--- webapp/config/themeConfig/themeCreator.js
+++ webapp/config/themeConfig/themeCreator.js
@@ -384,18 +384,22 @@ const themeCreator = (_themeVariables = {}, _themeConfig = {}) => {
     expandedRow: {
       container: {
         height: '100%',
+        background: themeColors.darkBg,
         ...makeGutter('padding', { all: 1 })
       },
       mainDataContainer: {
         display: 'flex',
-        flexDirection: 'column'
+        flexDirection: 'column',
+        flexWrap: 'wrap'
       },
       subDataContainer: {
         display: 'flex',
-        flexDirection: 'row'
+        flexDirection: 'row',
+        flexWrap: 'wrap'
       },
       rowHeader: {
-        width: '6rem'
+        width: '6rem',
+        margin: '0.8rem'
       },
       dataRow: {
         display: 'flex',
@@ -407,40 +411,13 @@ const themeCreator = (_themeVariables = {}, _themeConfig = {}) => {
         border: border2,
         overflow: 'auto',
         width: '80%',
-        ...makeGutter('padding', { all: 0.2 })
+        ...makeGutter('padding', { all: 0.2 }),
+        margin: 'auto',
+        textAlign: 'center'
       },
       expandVisualAid: {
         position: 'absolute',
         right: '1rem'
-      }
-    },
-
-    // tx table row
-    txTableRow: {
-      container: {
-        height: '100%',
-        background: themeColors.darkBg,
-        ...makeGutter('padding', { all: 1 })
-      },
-      thinRow: {
-        display: 'flex',
-        flexDirection: 'row',
-        marginBottom: '2px',
-        justifyContent: 'space-between',
-        ...makeGutter('padding', { all: 0.2 })
-      },
-      subThinRow: {
-        display: 'inline-flex'
-      },
-      subThinItem: {
-        ...makeGutter('padding', { all: 0.33 })
-      },
-      borderItem: {
-        border: border1,
-        borderStyle,
-        borderWidth,
-        borderRadius,
-        ...makeGutter('padding', { all: 0.33 })
       }
     },
 
@@ -535,8 +512,7 @@ const themeCreator = (_themeVariables = {}, _themeConfig = {}) => {
     expandedRow,
     tableRowStyle,
     tabMenu,
-    text,
-    txTableRow
+    text
   } = mergedThemeConfig;
 
   const styleSheet = {
@@ -554,7 +530,6 @@ const themeCreator = (_themeVariables = {}, _themeConfig = {}) => {
     expandedRow: StyleSheet.create(expandedRow),
     tabMenu: StyleSheet.create(tabMenu),
     text: StyleSheet.create(text),
-    txTableRow: StyleSheet.create(txTableRow),
     themeVariables: mergedThemeVariables,
     logoUrl,
     tableRowStyle
