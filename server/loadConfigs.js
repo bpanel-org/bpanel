8d7fa1eb09a854877308dbda8cca013e0245ddf6
Buck Perley <bucko.perley@gmail.com>
2018-08-21 16:07:16 -0700

    Load all client configs on the server (#74)
    * refactor loadConfigs to return array of client configs
    
    * cleanup w/ better use of configs
    
    * PR cleanup

--- server/loadConfigs.js
+++ server/loadConfigs.js
@@ -1,29 +1,81 @@
 const Config = require('bcfg');
+const fs = require('fs');
+const assert = require('bsert');
+const { resolve, parse } = require('path');
 
-function loadConfigs(configs) {
-  // Setup config object
-  // bPanel specific configs, will be prefixed with `BPANEL_` in env
-  // dataDir defaults to `~/.[MODULE_NAME]`, where MODULE_NAME
-  // is the first argument passed to the Config constructor
-  // (i.e. 'bpanel' below)
+// load main bpanel config
+function loadMainConfig(options = {}) {
   const config = new Config('bpanel');
   config.load({
+    env: true,
     argv: true,
-    env: true
+    arg: true
   });
 
   // load any custom configs being passed in
-  config.inject(configs);
+  config.inject(options);
 
-  // inject client configs from a config in the client dir
-  // pass `client-id` as a command line arg
-  // or as env var BPANEL_CLIENT_ID
-  // client dir is also configurable with `client-dir`
-  // or `BPANEL_CLIENT_DIR`
-  const clientDir = config.str('clients-dir', 'clients');
-  const clientId = config.str('client-id', 'default');
-  config.open(`${clientDir}/${clientId}.conf`);
   return config;
+}
+
+/*
+ * Get an array of configs for each client
+ * in the module's home directory
+ * @param {Object} [configs] - Object of custom configs to inject
+ * into the bcfg config object
+ * @returns {Config[]} An array of Config object which can be
+ * used to load clients
+ */
+function loadConfigs(_config) {
+  // first let's load the parent bpanel config
+  let config = _config;
+
+  // if not passed bcfg object, create one
+  if (!(_config instanceof Config)) config = loadMainConfig(_config);
+
+  // clientsDir is the folder where all client configs
+  // should be saved and can be changed w/ custom configs
+  const clientsDir = config.str('clients-dir', 'clients');
+
+  // get full path to client configs relative to the project
+  // prefix which defaults to `~/.bpanel`
+  const clientsPath = resolve(config.prefix, clientsDir);
+  const clientFiles = fs.readdirSync(clientsPath);
+
+  // ignore file names that start with a '.' such as
+  // system files and files without `.conf` extension
+  // then load config for that client
+  return clientFiles
+    .filter(name => name[0] !== '.' && /.conf$/.test(name))
+    .map(fileName => {
+      // After filter, we load bcfg object for each client
+
+      // id is the file name without the extension
+      const { name: clientId, ext } = parse(fileName);
+      assert(ext === '.conf', 'client configs must have .conf extension');
+      const clientConf = new Config(clientId);
+
+      // set an id in the conf if not set
+      // prefix for the client config should be a clients dir
+      // in the parent config's directory
+      clientConf.inject({
+        id: clientId,
+        prefix: `${config.prefix}/${clientsDir}`
+      });
+
+      // can pass configs to clients via argv and env
+      // will be the same for each config returned
+      clientConf.load({
+        argv: true,
+        env: true
+      });
+
+      // load configs from config file
+      // files are loaded from the prefix directory
+      clientConf.open(fileName);
+
+      return clientConf;
+    });
 }
 
 module.exports = loadConfigs;
