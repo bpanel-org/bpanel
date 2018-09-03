d083865e0edb5e8383c9a945b2c1ff33f74a9a2b
bucko13 <bucko.perley@gmail.com>
2018-09-03 09:27:45 -0700

    include configs directory in git

---
+++ configs/webpack.dll.config.js
@@ -0,0 +1,52 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const { DIST_DIR, MODULES_DIR } = require('./constants');
+
+const vendorManifest = path.join(DIST_DIR, '[name]-manifest.json');
+
+module.exports = {
+  target: 'web',
+  mode: 'production',
+  entry: {
+    vendor: [
+      'bcoin/lib/bcoin-browser',
+      'bcash/lib/bcoin-browser',
+      'hsd/lib/hsd-browser',
+      'bledger/lib/bledger-browser',
+      'bmultisig/lib/bmultisig-browser',
+      'hs-client',
+      'bclient',
+      'react',
+      'react-redux',
+      'react-dom',
+      '@bpanel/bpanel-ui',
+      '@bpanel/bpanel-utils'
+    ]
+  },
+  output: {
+    libraryTarget: 'umd',
+    path: DIST_DIR,
+    library: '[name]_lib',
+    filename: 'vendor.js'
+  },
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['-browser.js', '.js', '.json'],
+    // list of aliases are what packages plugins can list as peerDeps
+    // this helps simplify plugin packages and ensures that parent classes
+    // all point to the same instance, e.g bcoin.TX will be same for all plugins
+    alias: {
+      bcoin$: `${MODULES_DIR}/bcoin/lib/bcoin-browser`,
+      bcash$: `${MODULES_DIR}/bcash/lib/bcoin-browser`,
+      hsd$: `${MODULES_DIR}/hsd/lib/hsd-browser`,
+      'hs-client': `${MODULES_DIR}/hs-client`
+    }
+  },
+  plugins: [
+    new webpack.DllPlugin({
+      name: '[name]_lib',
+      path: vendorManifest
+    })
+  ]
+};
