3ec97700e92944f4798580c6966bd7997dd153db
bucko13 <bucko.perley@gmail.com>
2018-09-06 10:58:24 -0700

    can't use npm packages in preinstall

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,7 +1,7 @@
+/* eslint-disable no-console */
 const fs = require('fs');
 const os = require('os');
 const path = require('path');
-const logger = require('../server/logger');
 
 const BPANEL_DIR = path.resolve(os.homedir(), '.bpanel');
 const CONFIGS_FILE = path.resolve(BPANEL_DIR, './config.js');
@@ -15,33 +15,35 @@ const configText = `module.exports = {
 
 try {
   if (!fs.existsSync(BPANEL_DIR)) {
-    logger.info(`No module directory found. Creating one at ${BPANEL_DIR}`);
+    console.log(
+      `info: No module directory found. Creating one at ${BPANEL_DIR}`
+    );
     fs.mkdirSync(BPANEL_DIR);
   }
 
   if (!fs.existsSync(CONFIGS_FILE)) {
-    logger.info(
-      `No configuration file found. Initializing one at ${CONFIGS_FILE}`
+    console.log(
+      `info: No configuration file found. Initializing one at ${CONFIGS_FILE}`
     );
     fs.appendFileSync(CONFIGS_FILE, configText);
   }
 
   if (!fs.existsSync(LOCAL_PLUGINS_DIR)) {
-    logger.info(
-      `No local plugins directory file found. Creating one at ${LOCAL_PLUGINS_DIR}`
+    console.log(
+      `info: No local plugins directory file found. Creating one at ${LOCAL_PLUGINS_DIR}`
     );
     fs.mkdirSync(LOCAL_PLUGINS_DIR);
   }
 
   if (!fs.existsSync(CLIENTS_DIR)) {
-    logger.info(
-      `No clients directory file found. Creating one at ${CLIENTS_DIR}`
+    console.log(
+      `info: No clients directory file found. Creating one at ${CLIENTS_DIR}`
     );
     fs.mkdirSync(CLIENTS_DIR);
   }
 
   require('./version.js');
 } catch (e) {
-  logger.error('There was a problem initializing the project: ', e.stack);
+  console.error('There was a problem initializing the project: ', e.stack);
   process.exit(1);
 }
