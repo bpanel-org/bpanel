4dbd5a5522285e73b2007b4455b9f09b327a6274
bucko13 <bucko.perley@gmail.com>
2018-09-13 22:52:11 -0700

    testing scaffolding and splitting up webpack configs

--- server/socketManager.js
+++ server/socketManager.js
@@ -1,4 +1,5 @@
 const { Server } = require('bweb');
+const assert = require('bsert');
 
 class SocketManager extends Server {
   /**
@@ -7,7 +8,45 @@ class SocketManager extends Server {
    * @param {Object} options
    */
   constructor(options) {
-    super(options);
+    super(new SocketManagerOptions(options));
+    return this;
+    // this.init();
+  }
+}
+
+class SocketManagerOptions {
+  /**
+   * SocketManagerOptions
+   * @alias module:http.SocketManagerOptions
+   * @constructor
+   * @param {Object} options
+   */
+
+  constructor(options) {
+    this.fromOptions(options);
+  }
+
+  /**
+   * Inject properties from object.
+   * @private
+   * @param {Object} options
+   * @returns {SocketManagerOptions}
+   */
+
+  fromOptions(options) {
+    assert(options, 'Must pass options object');
+    return this;
+  }
+
+  /**
+   * Instantiate http options from object.
+   * @param {Object} options
+   * @returns {SocketManagerOptions}
+   */
+
+  static fromOptions(options) {
+    console.log('hello world');
+    return new SocketManagerOptions().fromOptions(options);
   }
 }
 
