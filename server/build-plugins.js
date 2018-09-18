43ef15e3a02976e51414ddcc5e986e24be0b1678
Jonathan Knapp <jonathan@purse.io>
2018-09-18 16:04:14 -0700

    Plugins can be overridden with cli or env configs

--- server/build-plugins.js
+++ server/build-plugins.js
@@ -272,7 +272,9 @@ your config file.'
     );
 
     const { localPlugins, plugins } = require(PLUGINS_CONFIG);
-    await prepareModules(plugins, false);
+    // CLI & ENV plugins override configuration file
+    const envPlugins = config.str('plugins');
+    await prepareModules(envPlugins ? envPlugins.split(',') : plugins, false);
     await prepareModules(localPlugins);
   } catch (err) {
     logger.error('There was an error preparing modules: ', err.stack);
