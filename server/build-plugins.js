a3426f41aa1838a27fc7234f2d4eb2f7e8ddc507
Jonathan Knapp <jonathan@purse.io>
2018-09-24 16:46:55 -0700

    Plugins config survives space padding

--- server/build-plugins.js
+++ server/build-plugins.js
@@ -274,7 +274,10 @@ your config file.'
     const { localPlugins, plugins } = require(PLUGINS_CONFIG);
     // CLI & ENV plugins override configuration file
     const envPlugins = config.str('plugins');
-    await prepareModules(envPlugins ? envPlugins.split(',') : plugins, false);
+    await prepareModules(
+      envPlugins ? envPlugins.split(',').map(s => s.trim(s)) : plugins,
+      false
+    );
     await prepareModules(localPlugins);
   } catch (err) {
     logger.error('There was an error preparing modules: ', err.stack);
