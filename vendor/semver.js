1b72404f16233a5f985de2de09ce755b5cb8c894
Mark Tyneway <mark@purse.io>
2018-11-28 10:08:30 -0800

    bugfix

--- vendor/semver.js
+++ vendor/semver.js
@@ -13,7 +13,7 @@
 // WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
 // ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
 // IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-// exports = module.exports = SemVer;
+exports = module.exports = SemVer;
 
 /* eslint-disable no-multi-spaces, indent */
 // The debug function is excluded entirely from the minified version.
