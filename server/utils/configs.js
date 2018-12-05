ee12f484e5c244fe138c43fcabcb27444ac7a5f5
Matthew Zipkin <pinheadmz@gmail.com>
2018-12-05 14:46:15 -0800

    configs: ignore argv (#140)
    * client configs: ignore argv
    
    * server: join client prefix path

--- server/utils/configs.js
+++ server/utils/configs.js
@@ -2,7 +2,7 @@
 
 const assert = require('bsert');
 const fs = require('bfile');
-const { resolve, parse } = require('path');
+const { resolve, parse, join } = require('path');
 const Config = require('bcfg');
 
 const pkg = require('../../pkg');
@@ -23,10 +23,14 @@ function loadConfig(name, options = {}) {
   config.inject(options);
 
   config.load({
-    env: true,
-    argv: true,
-    arg: true
+    env: true
   });
+
+  if (name === 'bpanel') {
+    config.load({
+      argv: true
+    });
+  }
 
   return config;
 }
@@ -95,7 +99,7 @@ function loadClientConfigs(_config) {
 
     const options = {
       id: clientId,
-      prefix: `${config.prefix}/${clientsDir}`
+      prefix: join(config.prefix, clientsDir)
     };
 
     const clientConf = loadConfig(clientId, options);
