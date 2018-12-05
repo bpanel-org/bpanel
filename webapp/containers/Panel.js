f8ffe6bf18b651033b6b576addf96e649cb6ec4d
Buck Perley <bucko.perley@gmail.com>
2018-12-05 11:19:54 -0800

    Panel: Loadable fix (#138)
    * fix lint command
    
    * remove react loadable

--- webapp/containers/Panel.js
+++ webapp/containers/Panel.js
@@ -1,16 +1,10 @@
 import { withRouter } from 'react-router';
 import { bindActionCreators } from 'redux';
-import Loadable from 'react-loadable';
-import { Spinner } from '@bpanel/bpanel-ui';
 
+import Panel from '../components/Panel';
 import { connect } from '../plugins/plugins';
 import { socketActions } from '../store/actions';
 import { nav } from '../store/selectors';
-
-const LoadablePanel = Loadable({
-  loader: () => import('../components/Panel'),
-  loading: Spinner
-});
 
 const mapStateToProps = state => ({
   paths: nav.uniquePathsByName(state),
@@ -21,10 +15,7 @@ const mapDispatchToProps = dispatch =>
   bindActionCreators({ ...socketActions }, dispatch);
 
 const PanelContainer = withRouter(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(LoadablePanel, 'Panel')
+  connect(mapStateToProps, mapDispatchToProps)(Panel, 'Panel')
 );
 
 export default PanelContainer;
