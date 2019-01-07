de680f11e8a4df75e9d3518e61928af7541e3db5
Buck Perley <bucko.perley@gmail.com>
2019-01-07 10:19:29 -0600

    package.json: update dependencies (#163)

--- configs/webpack.dll.config.js
+++ configs/webpack.dll.config.js
@@ -2,20 +2,12 @@ const webpack = require('webpack');
 const path = require('path');
 
 const { DIST_DIR, MODULES_DIR } = require('./constants');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const vendorManifest = path.join(DIST_DIR, '[name]-manifest.json');
 
 module.exports = {
   target: 'web',
-  mode: 'production',
-  optimization: {
-    minimizer: [
-      new UglifyJsPlugin({
-        parallel: true
-      })
-    ]
-  },
+  mode: 'development',
   entry: {
     vendor: [
       'bcoin/lib/bcoin-browser',
